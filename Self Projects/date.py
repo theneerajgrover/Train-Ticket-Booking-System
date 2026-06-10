7444a8ac74a5bfd2f20eb4a76ccd03af9207ed33
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 23:13:14 +0530

    Refactor date selection logic for better validation

--- Self Projects/date.py
+++ Self Projects/date.py
@@ -1,12 +1,26 @@
 def select_date():
-    while True :
-        date = (input("ENTER DATE : "))
-        month = (input("ENTER MONTH IN DIGITS : "))
-        if date.isdigit() and month.isdigit() :
-            if int(date) < 0 and int(month) < 0:
-                print("ENTER VALID DETAILS !!\n")
-            else :
-                break
+    while True:
+        date_input = input("ENTER DATE : ")
+        month_input = input("ENTER MONTH IN DIGITS : ")
+        
+        if date_input.isdigit() and month_input.isdigit():
+            day = int(date_input)
+            month = int(month_input)
             
-        else :
-            print("ENTER IN NUMERIC FORMAT !!")
+            if month >= 1 and month <= 12:
+                
+                max_days = 31
+                if month == 4 or month == 6 or month == 9 or month == 11:
+                    max_days = 30
+                elif month == 2:
+                    max_days = 29
+                
+                if day >= 1 and day <= max_days:
+                    print("\033[1m==== DATE ACCEPTED ====\033[0m")
+                    return day, month
+                else:
+                    print("\033[1m==== ENTER VALID DETAILS !! ====\n\033[0m")
+            else:
+                print("\033[1m==== ENTER VALID DETAILS !! ====\n\033[0m")
+        else:
+            print("\033[1m==== ENTER VALUES IN NUMERIC FORMAT !! ====\n\033[0m")
