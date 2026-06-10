ca41aa4c39d4a8c2fd36c06f23aa17e53b9b9250
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 23:12:45 +0530

    Enhance booking success messages with formatting

--- Self Projects/booking_successful.py
+++ Self Projects/booking_successful.py
@@ -1,3 +1,3 @@
 def booking():
-    print("Booking Successful !!")
-    print("THANKYOU FOR VISITING 🤗")
+    print("\033[1m\n==== Booking Successful !! ====\033[0m")
+    print("\033[1m==== THANKYOU FOR VISITING 🤗 ====\033[0m")
