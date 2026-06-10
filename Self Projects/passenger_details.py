ce9badb31c11fcfeec75278b24461bbc801a1593
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 16:27:54 +0530

    Enhance input validation for passenger details
    Refactor passenger details input for validation.

--- Self Projects/passenger_details.py
+++ Self Projects/passenger_details.py
@@ -1,8 +1,21 @@
 import seat_availability
-def details(seats) :
+
+def details(seats):
     passengers_list = {}
+    
     for i in range(seats):
-        name = input(f"ENTER NAME OF PASSENGER {i+1}: ")
-        age = int(input(f'ENTER AGE OF PASSENGER {i + 1}: '))
-        passengers_list[name] = age
+        print("\nENTER DETAILS CORRECTLY")
+        print("AGE should be an integer")
+        print("NAME must be a string")
+        
+        while True:
+            name = input(f"ENTER NAME OF PASSENGER {i+1}: ").strip()
+            age = input(f'ENTER AGE OF PASSENGER {i+1}: ').strip()
+            
+            if name.replace(" ", "").isalpha() and age.isdigit():
+                break
+            print("Invalid input! Please enter text for name and numbers for age.")
+        
+        passengers_list[f"Passenger_{i+1}"] = {"Name": name.upper(), "Age": int(age)}
+        
     return passengers_list
