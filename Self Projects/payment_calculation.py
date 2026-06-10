b4481b454b9d42b5caedf408a43bcb5be30ecdfd
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 15:43:31 +0530

    Refactor payment function to accept train and seats

--- Self Projects/payment_calculation.py
+++ Self Projects/payment_calculation.py
@@ -1,12 +1,11 @@
 import select_train, seat_availability
-def payment() :
+def payment(train, seats) :
     price = 0
     train_list = {"Orient Express" : 5100, "Maharajas' Express" : 3500, "Glacier Express" : 3100, "Vande Bharat Express" : 1800, "Shatabdi Express" : 1500}
-    select = select_train.train()
+    
     for i in train_list :
-        if select == i :
+        if train == i :
             price = train_list[i]
     
-    no_of_seats = seat_availability.seat_confirm()
-    amount = no_of_seats * price
+    amount = seats * price
     return amount
