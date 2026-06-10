a32e2521ab2c2b2a202f1410357b36d271c99a80
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 15:40:56 +0530

    Refactor seat availability and confirmation functions
    Refactor seat availability and confirmation logic to use a global seats variable.

--- Self Projects/seat_availability.py
+++ Self Projects/seat_availability.py
@@ -1,24 +1,29 @@
 import trains_list, select_train
+list1 = trains_list.trains
+seats = 0
 def seat_availability() :
-    seats = 0
-    train = select_train.train()
-    list1 = trains_list.trains_available()
+    global seats
+    my_train = select_train.selected()
     for i in list1 :
-        if train == i:
-            seats = list1[i]
+        if my_train == i:
+            seats = int(list1[i])
+            break
     return seats
 
-def seat_confirm() :
+
+def seat_confirm(seats) :
     while True :
         user_seats = int(input("ENTER NUMBER OF SEATS REQUIRED : "))
         if user_seats < 0 :
             print("ENTER VALID INPUT !!\n")
-        elif user_seats <= seat_availability() :
+        elif user_seats <= seats :
             print("SEATS AVAILABLE !!")
             confirmation = input("DO YOU WANT TO CONFIRM THIS COUNT ? (y/n) : ").lower()
             if confirmation == 'y' :
-                return user_seats
+                print(f"{user_seats} seats confirmed successfully")
+                break
             else :
                 print("ENTER AGAIN !!\n")
         else :
             print("THAT SEAT COUNT IS NOT AVAILABLE !!\n")
+            
