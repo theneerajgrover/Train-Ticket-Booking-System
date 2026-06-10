ff502a2d01120ee25c95f6334f34e5f2c5ef094b
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 23:13:51 +0530

    Enhance credential prompts with formatting

--- Self Projects/login.py
+++ Self Projects/login.py
@@ -2,10 +2,11 @@ def create_acc():
     id = input("CREATE A USERNAME : ")
     password = input("CREATE A PASSWORD : ")
     while True :
-        print("\nENTER YOUR CREDENTIALS NOW !!")
+        print("\033[1m\n==== ENTER YOUR CREDENTIALS NOW !! ====\033[0m")
         check_id = input("ENTER YOUR USERNAME : ")
         check_pass = input("ENTER YOUR PASSWORD : ")
         if check_id != id or check_pass != password :
-            print("ENTER VALID DETAILS !!")
+            print("\033[1m==== ENTER VALID DETAILS !! ====\033[0m")
+            
         else :
             break
