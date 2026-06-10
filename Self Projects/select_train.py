1ae8659e20c632a64ba15f110463ac570f1fc529
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 15:40:22 +0530

    Use global variable for selected train
    Refactor train selection to use a global variable for chosen train.

--- Self Projects/select_train.py
+++ Self Projects/select_train.py
@@ -1,15 +1,20 @@
 import trains_list
+chosen_train = ""
+
 def train():
-    selected = ""
-    dict_of_trains = trains_list.trains_available()
+    global chosen_train
+    dict_of_trains = trains_list.trains
     list_of_trains = list(dict_of_trains.keys())
     while True :
         selection = int(input("\nSELECT THE TRAIN YOU WANT TO TRAVEL IN : (1, 2, 3, 4, 5) : "))
         for train in dict_of_trains :
             if selection == (list_of_trains.index(train) + 1) :
-                print(train)
-                selected = train
+                chosen_train = train
                 return train
         
         else :
             print("INVALID INPUT !!\n")
+            
+def selected() :
+    return chosen_train
+    
