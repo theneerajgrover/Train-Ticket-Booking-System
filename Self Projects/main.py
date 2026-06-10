3305b04c27770587e51ee094df337ca3566fb860
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 23:14:25 +0530

    Improve output formatting and display passenger details
    Enhanced user interface with bold formatting and improved output for passenger details.

--- Self Projects/main.py
+++ Self Projects/main.py
@@ -1,10 +1,11 @@
 import login, route_selection, date, trains_list, select_train, seat_availability, payment_calculation, passenger_details, booking_successful
 
-print("WELCOME TO THE RAILWAY SEAT BOOKING APPLICATION !!")
+print("\033[1m" + "==== WELCOME TO THE RAILWAY SEAT BOOKING APPLICATION !! ====" + "\033[0m")
 
 login.create_acc()
 route_selection.select_start_end()
-date.select_date()
+
+tareek, month = date.select_date()
 trains = trains_list.trains_available()
 
 trains_list.print_list(trains)
@@ -13,7 +14,7 @@ select_train.train(trains)
 
 my_choice = select_train.selected()
 
-print(my_choice)
+print("\033[1m"+ my_choice + "\033[0m")
 seats = seat_availability.seat_availability(my_choice, trains)
 
 user_seats = seat_availability.seat_confirm(seats)
@@ -22,5 +23,8 @@ passengers = passenger_details.details(user_seats)
 # payment = payment_calculation.payment(my_choice, seats)
 
 # print("AMOUNT TO BE PAID :", payment)
-print(passengers)
+print("\033[1m\n==== DETAILS OF PASSENGERS ====\n\033[0m")
+
+for pas, details in passengers.items() :
+    print(pas, details)
 booking_successful.booking()
