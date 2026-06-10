d720482723b6e7518274d3c8126414b13ed8e00c
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 23:19:13 +0530

    Revise train list and enhance print output styling
    Updated the train list with new train names and improved print formatting for better visibility.

--- Self Projects/trains_list.py
+++ Self Projects/trains_list.py
@@ -1,13 +1,13 @@
 import random
 def trains_available() :
-    trains_list = {"Orient Express" : random.randint(1, 30) , "Maharajas' Express" : random.randint(1, 30) , "Glacier Express" : random.randint(1, 30) , "Vande Bharat Express" : random.randint(1, 30) , "Shatabdi Express" : random.randint(1, 30) }
-    # trains_list = {'Orient Express': 7, "Maharajas' Express": 27, 'Glacier Express': 2, 'Vande Bharat Express': 2, 'Shatabdi Express': 6}
+    trains_list = {"Dibrugarh - Kanyakumari Vivek Express" : random.randint(1, 30) , "Himsagar Express" : random.randint(1, 30) , "Aronai Superfast Express" : random.randint(1, 30) , "Navyug Express" : random.randint(1, 30) , "Avadh Assam Express" : random.randint(1, 30) }
     return trains_list
 
 def print_list(trains):
     list = []
     trains_list = trains
-    print("\nTHE AVAILABLE TRAINS ARE FOLLOWING !!\n")
+    print("\033[1m\n==== THE AVAILABLE TRAINS ARE FOLLOWING !! ====\n\033[0m")
+    
     for train in trains_list.items():
         list.append(train)
         print(train)
