034a7bf8f38f67880c7f0013723ae1049a410f3f
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 23:15:00 +0530

    Refactor passenger details input and validation
    Updated input prompts and validation for passenger details.

--- Self Projects/passenger_details.py
+++ Self Projects/passenger_details.py
@@ -1,20 +1,18 @@
-import seat_availability
-
 def details(seats):
     passengers_list = {}
+    print("\033[1m\n==== ENTER DETAILS CORRECTLY ====\033[0m")
+    print("\033[1m➡️AGE should be an integer\033[0m")
+    print("\033[1m➡️NAME must be a string\033[0m")
     
     for i in range(seats):
-        print("\nENTER DETAILS CORRECTLY")
-        print("AGE should be an integer")
-        print("NAME must be a string")
         
         while True:
-            name = input(f"ENTER NAME OF PASSENGER {i+1}: ").strip()
-            age = input(f'ENTER AGE OF PASSENGER {i+1}: ').strip()
+            name = input(f"\nENTER NAME OF PASSENGER {i+1}: ").strip()
+            age = input(f'ENTER AGE : ').strip()
             
-            if name.replace(" ", "").isalpha() and age.isdigit():
+            if name.replace(" ", "").isalpha() and age.isdigit() and 0 < int(age) <= 100:
                 break
-            print("Invalid input! Please enter text for name and numbers for age.")
+            print("\033[1m\n==== Invalid input! Please enter text for name and numbers for age. ====\033[0m")
         
         passengers_list[f"Passenger_{i+1}"] = {"Name": name.upper(), "Age": int(age)}
         
