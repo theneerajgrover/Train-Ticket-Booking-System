a6b133dd0fb962dd162616a3caf2ac47a17c21a0
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-10 16:29:58 +0530

    Modify trains_list to randomize train availability
    Updated trains_list to use random availability for trains.

--- Self Projects/trains_list.py
+++ Self Projects/trains_list.py
@@ -1,12 +1,10 @@
 import random
 def trains_available() :
-    # trains_list = {"Orient Express" : random.randint(1, 30) , "Maharajas' Express" : random.randint(1, 30) , "Glacier Express" : random.randint(1, 30) , "Vande Bharat Express" : random.randint(1, 30) , "Shatabdi Express" : random.randint(1, 30) }
-    trains_list = {'Orient Express': 7, "Maharajas' Express": 27, 'Glacier Express': 2, 'Vande Bharat Express': 2, 'Shatabdi Express': 6}
+    trains_list = {"Orient Express" : random.randint(1, 30) , "Maharajas' Express" : random.randint(1, 30) , "Glacier Express" : random.randint(1, 30) , "Vande Bharat Express" : random.randint(1, 30) , "Shatabdi Express" : random.randint(1, 30) }
+    # trains_list = {'Orient Express': 7, "Maharajas' Express": 27, 'Glacier Express': 2, 'Vande Bharat Express': 2, 'Shatabdi Express': 6}
     return trains_list
 
-trains = trains_available()
-
-def print_list():
+def print_list(trains):
     list = []
     trains_list = trains
     print("\nTHE AVAILABLE TRAINS ARE FOLLOWING !!\n")
