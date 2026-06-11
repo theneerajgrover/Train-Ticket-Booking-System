b46de614560356c7820ac342b747265239cb616e
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-11 17:31:46 +0530

    Remove seat confirmation logic from seat_availability.py
    Removed the seat_confirm function that handled user seat confirmation.

--- Self Projects/seat_availability.py
+++ Self Projects/seat_availability.py
@@ -8,23 +8,3 @@ def seat_availability(my_train, trains) :
             seats = int(list1[i])
             break
     return seats
-
-
-def seat_confirm(seats) :
-    while True :
-        user_seats = int(input("ENTER NUMBER OF SEATS REQUIRED : "))
-        if user_seats < 0 :
-            print("\033[1m\n==== ENTER VALID INPUT !! ====\033[0m")
-            
-        elif user_seats <= seats :
-            print("\033[1m\n==== SEATS AVAILABLE !! ====\033[0m")
-            
-            confirmation = input("DO YOU WANT TO CONFIRM THIS COUNT ? (y/n) : ").lower()
-            if confirmation == 'y' :
-                print("\033[1m" + f"==== {user_seats} seats confirmed successfully ====\033[0m")
-                break
-            else :
-                print("\033[1mENTER AGAIN !!\n\033[0m")
-        else :
-            print("\033[1m\n==== THAT SEAT COUNT IS NOT AVAILABLE !! ====\033[0m")
-    return user_seats
