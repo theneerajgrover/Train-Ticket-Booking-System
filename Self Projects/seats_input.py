a35dba5325a88b9bf31bc25204c2b0ae3902100b
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-11 21:00:05 +0530

    Refactor seat confirmation input validation

--- Self Projects/seats_input.py
+++ Self Projects/seats_input.py
@@ -1,19 +1,23 @@
 
 def seat_confirm(seats) :
     while True :
-        user_seats = int(input("ENTER NUMBER OF SEATS REQUIRED : "))
-        if user_seats < 0 :
-            print("\033[1m\n==== ENTER VALID INPUT !! ====\033[0m")
-            
-        elif user_seats <= seats :
-            print("\033[1m\n==== SEATS AVAILABLE !! ====\033[0m")
-            
-            confirmation = input("DO YOU WANT TO CONFIRM THIS COUNT ? (y/n) : ").lower()
-            if confirmation == 'y' :
-                print("\033[1m" + f"==== {user_seats} seats confirmed successfully ====\033[0m")
-                break
+        user_seats = (input("ENTER NUMBER OF SEATS REQUIRED : "))
+        if user_seats.isdigit() :
+            user_seats = int(user_seats)
+            if user_seats < 0 :
+                print("\033[1m\n==== ENTER VALID INPUT !! ====\033[0m")
+                
+            elif user_seats <= seats :
+                print("\033[1m\n==== SEATS AVAILABLE !! ====\033[0m")
+                
+                confirmation = input("DO YOU WANT TO CONFIRM THIS COUNT ? (y/n) : ").lower()
+                if confirmation == 'y' :
+                    print("\033[1m" + f"==== {user_seats} seats confirmed successfully ====\033[0m")
+                    break
+                else :
+                    print("\033[1mENTER AGAIN !!\n\033[0m")
             else :
-                print("\033[1mENTER AGAIN !!\n\033[0m")
+                print("\033[1m\n==== THAT SEAT COUNT IS NOT AVAILABLE !! ====\033[0m")
         else :
-            print("\033[1m\n==== THAT SEAT COUNT IS NOT AVAILABLE !! ====\033[0m")
+            print("\033[1mENTER A NUMBER !!\n\033[0m")
     return user_seats
