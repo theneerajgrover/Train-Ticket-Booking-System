573449a1dcd0ef351ed50624f4b01d996dd8ae67
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-11 21:00:25 +0530

    Implement user_selects_coach function
    Added user_selects_coach function to manage coach selection.

--- Self Projects/select_coach.py
+++ Self Projects/select_coach.py
@@ -13,3 +13,18 @@ def generate_coach_seats(coaches, total_seats):
         remaining_seats -= 1
 
     return coach_seats
+
+def user_selects_coach(coach_dict, count) :
+    coaches = coach_dict
+    list_for_coaches = []
+    for i in range(count):
+        for i in coaches :
+            while True :
+                if i not in list_for_coaches :
+                    list_for_coaches.append(i)
+                if list_for_coaches.count(i) < coaches[i]:
+                    list_for_coaches.append(i)
+                else :
+                    break
+                
+    return(list_for_coaches)
