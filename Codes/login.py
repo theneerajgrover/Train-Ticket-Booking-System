631ad4cbe47166baeb8d6a1360f3ffebfe984f54
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-12 19:38:24 +0530

    Enhance login functionality with user validation
    Updated login process to check for existing users and handle account creation.

--- Codes/login.py
+++ Codes/login.py
@@ -1,12 +1,37 @@
-def create_acc():
-    id = input("CREATE A USERNAME : ")
-    password = input("CREATE A PASSWORD : ")
+import create_account
+
+def process() :
     while True :
-        print("\033[1m\n==== ENTER YOUR CREDENTIALS NOW !! ====\033[0m")
-        check_id = input("ENTER YOUR USERNAME : ")
-        check_pass = input("ENTER YOUR PASSWORD : ")
-        if check_id != id or check_pass != password :
-            print("\033[1m==== ENTER VALID DETAILS !! ====\033[0m")
-            
-        else :
-            break
+        username = input("\nENTER YOUR USERNAME : ")
+        
+        user_found = False  
+        with open("users_cred.txt", "r") as file :
+            for user in file :
+                user,pswd = user.strip().split(",")
+                if username == user :
+                    user_found = True
+                    break
+        
+        if not user_found:
+            print("\033[1m" + "\n==== ACCOUNT DOESN'T EXIST ====" + "\033[0m")
+            choice = input("DO YOU WANT TO CREATE A NEW ACCOUNT ? (y / n) : ").lower()
+            if choice == 'y' :
+                create_account.process()
+                return 
+            else:
+                continue
+                    
+        password = input("ENTER YOUR PASSWORD : ")
+        
+        with open("users_cred.txt", "r") as file :
+            for line in file :
+                user, pswd = line.strip().split(",")
+                
+                if username == user:
+                    if password == pswd :
+                        print("\033[1m" + "\n==== LOGIN SUCCESSFUL ====" + "\033[0m")
+                        return username
+                    
+                    else :
+                        print("\033[1m\n==== INCORRECT PASSWORD !! ====\n\033[1m")
+                        break
