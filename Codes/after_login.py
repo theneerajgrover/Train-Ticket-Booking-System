a8f6d44839fc002dffe6778d7b8741662d381f5a
Neeraj Grover <theneerajgrover@gmail.com>
2026-06-14 23:15:00 +0530

    Streamline ticket booking process in after_login
    Refactor after_login function to enhance ticket booking flow and improve user experience.

--- Codes/after_login.py
+++ Codes/after_login.py
@@ -1,33 +1,64 @@
-import cancel_ticket, booking_history, exit, book_ticket
+import login, route_selection, date, trains_list, select_train, coaches_list, select_coach, seat_availability, seats_input, payment_calculation, passenger_details, booking_successful
 
-user_logged_in = False
+first_thing_done = True
 
-def after_login(username) :
+
+def book_ticket(user_logged_in):
     
-    global user_logged_in
+    global first_thing_done, username
+    username = login.logged_in
     
     while True :
-        print("\033[1m==== HEY", username + " ====\n\033[0m")
-        print("1. Book Train Ticket")
-        print("2. Cancel Train Ticket")
-        print("3. Booking History")
-        print("4. Exit")
         
-        user = input("\033[1mENTER YOUR CHOICE : (1,2,3,4) : \033[0m")
-        if user == "1" :
-            user_logged_in = True
-            book_ticket.book_ticket(user_logged_in)
-            
-        elif user == "2":
-            cancel_ticket.process(username)
-            pass
-            
-        elif user == "3" :
-            booking_history.process(username)
-            
-        elif user == "4":
-            exit.exit_program()
+        if first_thing_done :
+            route_selection.select_start_end()
 
-        else :
-            print("\033[1m==== INVALID INPUT !! ====\n\033[1m")
-    
+            tareek, month = date.select_date()
+            trains = trains_list.trains_available()
+
+            trains_list.print_list(trains)
+
+            select_train.train(trains)
+
+            my_choice = select_train.selected()
+
+            print("\033[1m"+ my_choice + "\033[0m")
+            seats = seat_availability.seat_availability(my_choice, trains)
+
+            coaches_available = coaches_list.coaches()
+
+            coach_for_selected_train = coaches_list.print_coaches(coaches_available, my_choice)
+            coach_list_with_seats = select_coach.generate_coach_seats(coach_for_selected_train, seats)
+            print()
+            print(coach_list_with_seats)
+            
+            first_thing_done = False
+            
+        if not user_logged_in :
+            
+            print("\033[1m" + "==== LOGIN IS REQUIRED TO BOOK TICKETS ====\033[0m")
+            username = login.process()
+            user_logged_in = True
+        
+        if user_logged_in :
+            if not first_thing_done :
+                
+                print("\033[1m" + "==== Hey",username, "====\033[0m")
+                user_seats = seats_input.seat_confirm(seats)
+
+                chosen_coach = select_coach.user_selects_coach(coach_list_with_seats, user_seats)
+                # print(chosen_coach)
+
+                passengers = passenger_details.details(tareek, month, user_seats, chosen_coach, username)
+
+                # seat_number = passenger_details.generate_seat_number(chosen_coach, user_seats)
+                # payment = payment_calculation.payment(my_choice, seats)
+
+                # print("AMOUNT TO BE PAID :", payment)
+                print("\033[1m\n==== DETAILS OF PASSENGERS ====\n\033[0m")
+
+                for pas, details in passengers.items() :
+                    print(pas, details)
+                    
+                booking_successful.booking()
+                return username
